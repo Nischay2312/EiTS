1b65563e667ad6454aa68f0bafc0e06255b53359
Nischay Joshi <nischayjoshi2312@gmail.com>
2023-10-04 21:05:16 -0700

    Setup for the System, can play video and audio from SD card

---
+++ src/Main/config.h
@@ -0,0 +1,35 @@
+// /////////////////////////////////////////////////////////////////
+// /*
+//   Esp infoTainment System (EiTS)
+//   For More Information: https://github.com/Nischay2312/EiTS
+//   Created by Nischay J., 2023
+// */
+// /////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////
+// /*
+//   config.h
+//   Header file that hosts all the pin definitions for the project.
+// */
+// /////////////////////////////////////////////////////////////////
+
+//ST7735 TFT Display Pinouts
+#define CS 5
+#define SCK 18
+#define MOSI 23
+#define MISO -1
+#define DC 27
+#define RST 33
+#define BLK 22
+
+//I2S Audio Driver Pinouts
+#define I2S_MCLK -1
+#define I2S_SCLK 25
+#define I2S_LRCLK 26
+#define I2S_DOUT 32
+#define I2S_DIN -1
+
+//SD Card Pinouts
+#define SD_SCK 14
+#define SD_MOSI 15
+#define SD_MISO 4
+#define SD_CS 13
---
+++ src/Main/version.h
@@ -0,0 +1,19 @@
+// /////////////////////////////////////////////////////////////////
+// /*
+//   Esp infoTainment System (EiTS)
+//   For More Information: https://github.com/Nischay2312/EiTS
+//   Created by Nischay J., 2023
+// */
+// /////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////
+// /*
+//   version.h
+//   Header file that hosts all the version information for the project.
+// */
+// /////////////////////////////////////////////////////////////////
+#define APP_NAME "video_player"
+#define K_VIDEO_INDEX "video_index"
+#define BASE_PATH "/Videos/"
+#define AAC_FILENAME "/test.aac"
+#define MJPEG_FILENAME "/test.mjpeg"
+#define VIDEO_COUNT 20
