d8d488e59cb0b633932ed7fb6a1d49bda4483903
Nischay Joshi <nischayjoshi2312@gmail.com>
2023-10-21 23:13:04 -0700

    Revert "Reverted python convert videos program"
    This reverts commit a7aea136acc675a32bece6b3dcfa716246397d5e.

--- python/convertVideos/ConvertVideos.py
+++ python/convertVideos/ConvertVideos.py
@@ -19,6 +19,8 @@ class VideoConverterApp:
         self.output_folder_var = StringVar()
         self.output_name_var = StringVar()
         self.start_num_var = IntVar()
+        self.quality_var = IntVar()
+        self.quality_var.set(6)  # Default value
 
         # Layout
         tk.Label(root, text="Input Folder:").grid(row=0, column=0, sticky=tk.W, padx=10, pady=5)
@@ -35,23 +37,29 @@ class VideoConverterApp:
         tk.Label(root, text="Starting Number:").grid(row=3, column=0, sticky=tk.W, padx=10, pady=5)
         tk.Entry(root, textvariable=self.start_num_var, width=40).grid(row=3, column=1, padx=10, pady=5)
 
-        tk.Button(root, text="Convert", command=self.start_conversion_thread, width=20).grid(row=4, column=0, padx=10, pady=20)
-        tk.Button(root, text="Repeat", command=self.reset_gui, width=10).grid(row=4, column=1, padx=10, pady=20)
-        tk.Button(root, text="Exit", command=root.quit, width=10).grid(row=4, column=2, padx=10, pady=20)
+        # New Layout Entry for Quality
+        tk.Label(root, text="Quality (0-31):").grid(row=4, column=0, sticky=tk.W, padx=10, pady=5)
+        tk.Entry(root, textvariable=self.quality_var, width=40).grid(row=4, column=1, padx=10, pady=5)
 
-        tk.Label(root, text="CONSOLE OUTPUT", font=("Arial", 12, "bold"), anchor="w").grid(row=5, column=0, columnspan=3, padx=10, pady=5, sticky="w")
+        # Adjusting the grid positioning of other elements to accommodate the new entry
+        tk.Button(root, text="Convert", command=self.start_conversion_thread, width=20).grid(row=5, column=0, padx=10, pady=20)
+        tk.Button(root, text="Repeat", command=self.reset_gui, width=10).grid(row=5, column=1, padx=10, pady=20)
+        tk.Button(root, text="Exit", command=root.quit, width=10).grid(row=5, column=2, padx=10, pady=20)
 
-        self.console = tk.Text(root, height=10, width=50)
-        self.console.grid(row=7, column=0, columnspan=3, padx=10, pady=10, sticky='nsew')
-
-        self.console_scrollbar = tk.Scrollbar(root, command=self.console.yview)
-        self.console_scrollbar.grid(row=7, column=3, sticky='nsew')
-        self.console['yscrollcommand'] = self.console_scrollbar.set
-                
-        tk.Button(root, text="Clear Console", command=self.clear_console, width=15).grid(row=8, column=2, padx=10, pady=5)
+        tk.Label(root, text="CONSOLE OUTPUT", font=("Arial", 12, "bold"), anchor="w").grid(row=6, column=0, columnspan=3, padx=10, pady=5, sticky="w")
         
         self.progress = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=300, mode='determinate')
-        self.progress.grid(row=6, column=0, columnspan=3, padx=(10, 0), pady=20, sticky= "ew")
+        self.progress.grid(row=7, column=0, columnspan=3, padx=(10, 0), pady=20, sticky= "ew")
+
+        self.console = tk.Text(root, height=10, width=50)
+        self.console.grid(row=8, column=0, columnspan=3, padx=10, pady=10, sticky='nsew')
+
+        self.console_scrollbar = tk.Scrollbar(root, command=self.console.yview)
+        self.console_scrollbar.grid(row=8, column=3, sticky='nsew')
+        self.console['yscrollcommand'] = self.console_scrollbar.set
+                
+        tk.Button(root, text="Clear Console", command=self.clear_console, width=15).grid(row=9, column=2, padx=10, pady=5)
+        
 
     def start_conversion_thread(self):
         thread = threading.Thread(target=self.convert_videos)
@@ -75,6 +83,7 @@ class VideoConverterApp:
         output_folder = self.output_folder_var.get()
         output_name = self.output_name_var.get()
         start_num = self.start_num_var.get()
+        quality = str(self.quality_var.get())
 
         self.progress["maximum"] = len(os.listdir(input_folder))
         self.progress["value"] = 0
@@ -99,8 +108,7 @@ class VideoConverterApp:
                         self.console.insert(tk.END, line)
                         self.console.see(tk.END)  # Auto-scrolling
                     
-                    process = subprocess.Popen(['ffmpeg', '-i', input_file, '-y', '-vf', 'fps=24,scale=160:128:flags=lanczos', '-q:v', '6', output_video], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-                    for line in process.stdout:
+                        process = subprocess.Popen(['ffmpeg', '-i', input_file, '-y', '-vf', 'fps=24,scale=160:128:flags=lanczos', '-q:v', quality, output_video], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
                         self.console.insert(tk.END, line)
                         self.console.see(tk.END)  # Auto-scrolling
 
